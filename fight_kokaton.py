654b8aa36da81496d269d6030706537ea8108176
Fujinuma <c0a22125e5@edu.teu.ac.jp>
2023-12-05 17:00:52 +0900

    演習２：ビームの向きをこうかとんの向きに応じて変更

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -2,6 +2,7 @@ import os
 import random
 import sys
 import time
+import math
 
 import pygame as pg
 
@@ -58,6 +59,7 @@ class Bird:
         self.img = self.imgs[(+5,0)]
         self.rct = self.img.get_rect()
         self.rct.center = xy
+        self.dire = (+5, 0)
 
     def change_img(self, num: int, screen: pg.Surface):
         """
@@ -85,6 +87,9 @@ class Bird:
         if not (sum_mv[0] == 0 and sum_mv[1] == 0):  # 練習3：こうかとんの画像を向きを更新
             self.img = self.imgs[tuple(sum_mv)]
         screen.blit(self.img, self.rct)
+        if not (sum_mv[0] == 0 and sum_mv[1] == 0):
+            self.img = self.imgs[tuple(sum_mv)]
+            self.dire = tuple(sum_mv)  # 方向を更新
 
 
 class Bomb:
@@ -131,10 +136,22 @@ class Beam:
         self.rct.centerx = bird.rct.centerx + bird.rct.width/1.5  # ビームの初期X座標をこうかとんの中心X座標から少し右に設定
         self.rct.centery = bird.rct.centery  # ビームの初期Y座標をこうかとんの中心Y座標に設定
         self.vx, self.vy = +5, 0
+
+        # こうかとんの方向からビームの方向を取得
+        vx, vy = bird.dire
+        # 画像の回転
+        angle_rad = math.atan2(vy, vx)
+        angle_deg = math.degrees(angle_rad)
+        self.img = pg.transform.rotate(self.img, -angle_deg)
+        
+        # 初期位置の調整
+        self.rct.centerx = bird.rct.centerx + bird.rct.width * vx / 5
+        self.rct.centery = bird.rct.centery + bird.rct.height * vy / 5
+        self.vx, self.vy = vx, vy
     
     def update(self, screen: pg.Surface):
         """
-        ビームを速度ベクトルself.vx, self.vyに基づき移動させる
+        ビームを速度vxにしたがって移動させる
         引数 screen：画面Surface
         """
         self.rct.move_ip(self.vx, self.vy)
