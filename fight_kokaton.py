2e7e5f247b3f3d54155b43b076818f937b656a1c
Fujinuma <c0a22125e5@edu.teu.ac.jp>
2023-12-05 14:10:40 +0900

    練習１：ビームクラス追加

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -42,18 +42,18 @@ class Bird:
         引数1 num：こうかとん画像ファイル名の番号
         引数2 xy：こうかとん画像の位置座標タプル
         """
-        # img0 = pg.transform.rotozoom(pg.image.load(f"{MAIN_DIR}/fig/{num}.png"), 0, 2.0)
-        # img = pg.transform.flip(img0, True, False)  # デフォルトのこうかとん（右向き）
-        # self.imgs = {  # 0度から反時計回りに定義
-        #     (+5, 0): img,  # 右
-        #     (+5, -5): pg.transform.rotozoom(img, 45, 1.0),  # 右上
-        #     (0, -5): pg.transform.rotozoom(img, 90, 1.0),  # 上
-        #     (-5, -5): pg.transform.rotozoom(img0, -45, 1.0),  # 左上
-        #     (-5, 0): img0,  # 左
-        #     (-5, +5): pg.transform.rotozoom(img0, 45, 1.0),  # 左下
-        #     (0, +5): pg.transform.rotozoom(img, -90, 1.0),  # 下
-        #     (+5, +5): pg.transform.rotozoom(img, -45, 1.0),  # 右下
-        # }
+        #img0 = pg.transform.rotozoom(pg.image.load(f"{MAIN_DIR}/fig/{num}.png"), 0, 2.0)
+        #img = pg.transform.flip(img0, True, False)  # デフォルトのこうかとん（右向き）
+        #self.imgs = {  # 0度から反時計回りに定義
+        #    (+5, 0): img,  # 右
+        #    (+5, -5): pg.transform.rotozoom(img, 45, 1.0),  # 右上
+        #    (0, -5): pg.transform.rotozoom(img, 90, 1.0),  # 上
+        #    (-5, -5): pg.transform.rotozoom(img0, -45, 1.0),  # 左上
+        #    (-5, 0): img0,  # 左
+        #    (-5, +5): pg.transform.rotozoom(img0, 45, 1.0),  # 左下
+        #    (0, +5): pg.transform.rotozoom(img, -90, 1.0),  # 下
+        #    (+5, +5): pg.transform.rotozoom(img, -45, 1.0),  # 右下
+        #}
         self.img = pg.transform.flip(  # 左右反転
             pg.transform.rotozoom(  # 2倍に拡大
                 pg.image.load(f"{MAIN_DIR}/fig/{num}.png"), 
@@ -122,12 +122,33 @@ class Bomb:
         screen.blit(self.img, self.rct)
 
 
+class Beam:
+    """
+    ビームに関するクラス
+    """
+    def __init__(self, bird: Bird):
+        self.img = pg.image.load(f"{MAIN_DIR}/fig/beam.png")
+        self.rct = self.img.get_rect()
+        self.rct.center = bird.rct.center  # ビームの初期座標をこうかとんの中心座標に設定
+        self.vx, self.vy = +5, 0
+    
+    def update(self, screen: pg.Surface):
+        """
+        ビームを速度ベクトルself.vx, self.vyに基づき移動させる
+        引数 screen：画面Surface
+        """
+        self.rct.move_ip(self.vx, self.vy)
+        screen.blit(self.img, self.rct)
+
+
+
 def main():
     pg.display.set_caption("たたかえ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))    
     bg_img = pg.image.load(f"{MAIN_DIR}/fig/pg_bg.jpg")
     bird = Bird(3, (900, 400))
     bomb = Bomb((255, 0, 0), 10)
+    beam = None
 
     clock = pg.time.Clock()
     tmr = 0
@@ -135,6 +156,8 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
+            if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:  # スペースキーが押されたら
+                beam = Beam(bird)  # ビームインスタンスを生成
         
         screen.blit(bg_img, [0, 0])
         
@@ -148,6 +171,8 @@ def main():
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
         bomb.update(screen)
+        if beam is not None:
+            beam.update(screen)
         pg.display.update()
         tmr += 1
         clock.tick(50)
